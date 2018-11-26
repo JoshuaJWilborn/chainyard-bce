b2e0893ef09fdc806998d38d352235fc28fa5296
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2018-11-26 13:51:03 -0500

    build folder copied to top directory

---
+++ precache-manifest.f5ce0da62299221104acd35c234faa74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "406d566329a3c8ba5658",
+    "url": "/chainyard-bce/static/js/runtime~main.406d5663.js"
+  },
+  {
+    "revision": "1383164540fc761e4d78",
+    "url": "/chainyard-bce/static/js/main.13831645.chunk.js"
+  },
+  {
+    "revision": "1ac81abc31d862948e0e",
+    "url": "/chainyard-bce/static/js/1.1ac81abc.chunk.js"
+  },
+  {
+    "revision": "1383164540fc761e4d78",
+    "url": "/chainyard-bce/static/css/main.e72251f9.chunk.css"
+  },
+  {
+    "revision": "3ad1f5481461e0429cc8caded4e71b41",
+    "url": "/chainyard-bce/index.html"
+  }
+];
